919cb80a2724305f7d0e319692bf58e1f79d76cd
Matheus Rezende <matheus.rezende@unimedanapolis.com.br>
2025-10-08 14:42:00 -0300

    Remoção da autenticação dos controllers para teste dos CRUDs

--- faculdadeprojeto/src/main/java/com/progwebii/faculdadeprojeto/controller/UsuarioController.java
+++
@@ -1,4 +0,0 @@
-package com.progwebii.faculdadeprojeto.controller;
-
-public class UsuarioController {
-}
--- faculdadeprojeto/src/main/java/com/progwebii/faculdadeprojeto/security/SecurityConfig.java
+++ faculdadeprojeto/src/main/java/com/progwebii/faculdadeprojeto/security/SecurityConfig.java
@@ -32,7 +32,11 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/login", "/register", "/css/**").permitAll()
+                        .requestMatchers(
+                                "/login",
+                                "/register",
+                                "/css/**",
+                                "api/**").permitAll()
                         .anyRequest().authenticated()
                 )
                 .formLogin(form -> form
